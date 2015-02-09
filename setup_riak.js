50c5abe81d3eefb3af0f6453c13524aba2895de1
Corentin Jechoux <corentin.jechoux.pro@gmail.com>
2015-02-09 22:46:46 +0100

    add script to replace the original riak setup

---
+++ setup_riak.js
@@ -0,0 +1,28 @@
+var fs = require( 'fs' );
+var path = require( 'path' );
+
+/**
+ * rename a file with the '.back' extension
+ */
+function backupFile( filename ) {
+	var target = filename + '.back';
+	if ( !fs.existsSync( target ) ) {
+		fs.renameFileSync( filename, target );
+	}
+}
+/**
+ * copy a file into a folder. the original target file is backed up
+ */
+function copy( filename, folder ) {
+	var target = folder + '/' + path.basename( filename );
+	backupFile( target );
+	var options = {
+		mode: '0777'
+	};
+	fs.createReadStream( filename ).pipe( fs.createWriteStream( target, options ) );
+}
+
+
+copy( __dirname + '/riak_script/riak', '/app/.dpkg/usr/sbin' );
+copy( __dirname + '/riak_script/env.sh', '/app/.dpkg/usr/sbin' );
+copy( __dirname + '/riak_script/riak.conf', '/app/.dpkg/usr/sbin' );
