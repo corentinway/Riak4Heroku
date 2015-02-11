d482a55c0ae7c883ce459236537399cf0b77eb68
Corentin Jechoux <corentin.jechoux.pro@gmail.com>
2015-02-11 23:31:51 +0100

    rename riak.conf to riak.config

--- setup_riak.js
+++ setup_riak.js
@@ -27,4 +27,4 @@ console.log( 'setting up riak from: ' + __dirname );
 
 copy( __dirname + '/riak_script/riak', __dirname + '/.dpkg/usr/sbin' );
 copy( __dirname + '/riak_script/env.sh', __dirname + '/.dpkg/usr/lib/riak/lib/' );
-copy( __dirname + '/riak_script/riak.conf', __dirname + '/.dpkg/etc/riak' );
+copy( __dirname + '/riak_script/riak.config', __dirname + '/.dpkg/etc/riak' );
