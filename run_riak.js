305651ea12e0627d04ee8237c74dcb2be81aa8c4
Corentin Jechoux <corentin.jechoux.pro@gmail.com>
2015-02-20 07:45:47 +0100

    remove the configuration function from here and move them into a .profile.d file

--- run_riak.js
+++ run_riak.js
@@ -4,63 +4,7 @@
 
 var fs = require( 'fs' );
 var exec = require( 'child_process' ).exec;
-var split = require( 'split' );
-var through = require( 'through' );
 
-var configurationFilePath = __dirname + '/.dpkg/etc/riak/riak.conf';
-
-if ( !fs.existsSync( configurationFilePath ) ) {
-	console.error( 'Configuration file path does not exists: ' + configurationFilePath );
-	process.exit( 1 );
-}
-
-
-// TODO move that in ~/.profile.d
-
-var configurations = [
-	{ key: 'listener.http.internal', value: 'listener.http.internal = 127.0.0.1:' + process.env.PORT }
-];
-/**
- * update one line of the configuration file
- */
-var updateConfigurationLine = function ( line ) {
-	var i;
-	for ( i = 0; i < configurations.length; i++ ) {
-		console.log( '  checking configuration line: ' + line  );
-		if ( line.indexOf( configurations[ i ].key ) >= 0 ) {
-			console.log( '  line to change: ' + line );
-			this.queue( '# --- configuration changed for the key "' + configurations[ i ].key + '" for the old  value : ' + line + '\n' );
-			this.queue( configurations[ i ].value + '\n' );
-			return;
-		}
-	}
-	this.queue( line + '\n' );
-};
-/**
- * update the configuration file
- */
-var updateConfiguration = function( callback ) {
-	console.log( '  Updating configuration' );
-	console.log( '------------------------' );
-	fs.createReadStream( configurationFilePath )
-	.pipe( split( '\n') )
-	.pipe( through( updateConfigurationLine ) )
-	.pipe( fs.createWriteStream( configurationFilePath ) )
-	.on( 'end', callback );
-};
-
-var showConfiguration = function( callback ) {
-	console.log( 'Riak configuration' );
-	console.log( '------------------' );
-	fs.createReadStream( configurationFilePath )
-	.pipe( split( '\n') )
-	.on( 'data', function ( data ) {
-		if ( data.trim().indexOf( '#') !== 0 ) {
-			console.log( data.trim() );
-		}
-	} )
-	.on( 'end', callback );
-};
 
 var execHandler = function ( errorMessage, callback  ) {
 	return function ( error, stdout, stderr ) {
@@ -98,11 +42,7 @@ var pings = function () {
 };
 
 var start = function () {
-	updateConfiguration( function () {
-		showConfiguration( function () {
-			exec( __dirname + '/.dpkg/usr/sbin/riak start' , execHandler( 'Failed to start Riak.', pings ) );
-		} );
-	} );
+	exec( __dirname + '/.dpkg/usr/sbin/riak start' , execHandler( 'Failed to start Riak.', pings ) );
 };
 
 var stop = function () {
