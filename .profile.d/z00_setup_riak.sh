89686e763064d656c11e85f29daa72c2eb96c547
Corentin Jechoux <corentin.jechoux.pro@gmail.com>
2015-02-20 17:53:55 +0100

    change path to riak.conf to absolute path

--- .profile.d/z00_setup_riak.sh
+++ .profile.d/z00_setup_riak.sh
@@ -2,4 +2,4 @@
 
  # change the setting : listener.http.internal = 127.0.0.1:_LISTENER_HTTP_INTERNAL_PORT_
 
-sed -i 's/_LISTENER_HTTP_INTERNAL_PORT_/'"$PORT"'/' ../.dpkg/etc/riak/riak.conf
+sed -i 's/_LISTENER_HTTP_INTERNAL_PORT_/'"$PORT"'/' ~/.dpkg/etc/riak/riak.conf
