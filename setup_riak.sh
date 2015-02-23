fa5f8226634e51ff2738a13b8b1638788ad6664d
Corentin Jechoux <corentin.jechoux.pro@gmail.com>
2015-02-23 17:44:36 +0100

    add a new setup script

---
+++ setup_riak.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "backup files"
+# backup files 
+cp .dpkg/usr/sbin/riak              .dpkg/usr/sbin/riak.back
+cp .dpkg/usr/lib/riak/lib/env.sh    .dpkg/usr/lib/riak/lib/env.sh.back
+cp .dpkg/etc/riak/riak.conf         .dpkg/etc/riak/riak.conf.back
+# deploy new files
+echo "deploy new files"
+cp riak_script/riak        .dpkg/usr/sbin/riak
+cp riak_script/env.sh      .dpkg/usr/lib/riak/lib/env.sh
+cp riak_script/riak.conf   .dpkg/etc/riak/riak.conf
